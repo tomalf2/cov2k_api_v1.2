f3c86ed97b9c4a15230e7117f7ee7cbb59e8b3cf
tomalf2 <tommaso.alfonsi@polimi.it>
2021-11-23 10:51:43 +0100

    deletes unused files + launch script for production env

--- start_api_server_beanie_production.sh
+++ start_api_server_beanie_production.sh
@@ -3,5 +3,5 @@
 source ~/miniconda3/etc/profile.d/conda.sh  # finds command conda
 conda activate cov2k_api_v1
 
-uvicorn main_beanie:app
+uvicorn main_beanie:app --host=localhost --root-path /cov2k/api
 
