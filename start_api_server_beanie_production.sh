5c7c24c300fabb31864f4a16b06fde783490aa10
tomalf2 <tommaso.alfonsi@polimi.it>
2021-12-02 15:25:52 +0100

    automates logging generation

--- start_api_server_beanie_production.sh
+++ start_api_server_beanie_production.sh
@@ -3,5 +3,10 @@
 source ~/miniconda3/etc/profile.d/conda.sh  # finds command conda
 conda activate cov2k_api_v1
 
-uvicorn main_beanie:app --host=localhost --root-path /cov2k/api
+# define utility functions
+timestamp() {
+  TZ=Europe/Rome date +"%Y_%m_%d__%H_%M_%S" # current time
+}
+
+uvicorn main_beanie:app --host=localhost --root-path /cov2k/api 2>&1 | tee "log_$(timestamp).txt"
 
